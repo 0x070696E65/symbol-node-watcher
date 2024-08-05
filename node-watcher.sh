11e6f0e385dcad87a0b41450fd33aef09ffc0596
0x070696E65 <matsukawa5955@gmail.com>
2024-08-05 05:54:16 +0200

    不要なログの削除

--- node-watcher.sh
+++ node-watcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 LOG_DIR=`pwd`
-LOG_FILE="$LOG_DIR/symbolNodeWatcher.log"
+LOG_FILE="$LOG_DIR/logfile.log"
 nohup node $SCRIPT_DIR/dist/symbol-node-watcher.js "$@" >> $LOG_FILE 2>&1 &
